c03e0112c82923bed609e324f5d6fb275f76e4e8
harshasanjeeva <harshasanjiva@gmail.com>
2018-03-01 18:40:03 -0800

    adding reactstrap

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { Route, withRouter } from 'react-router-dom';
 // import text from './../images/1.svg';
 // import logo from './../images/2.svg';
 // import Sigin from './Signin'
 
-class NavHeader extends React.Component {
-  constructor(props) {
+class NavHeader extends Component {
+//   constructor(props) {
     // super(props);
     // this.toggle = this.toggle.bind(this);
     // this.state = {
     //   isOpen: false
     // };
-  }
+//   }
 
 //   toggle() {
 //     this.setState({
@@ -24,7 +24,7 @@ class NavHeader extends React.Component {
       <div  className="col-md-12">
         <Navbar color="faded" light expand="md">
           <NavbarBrand href="/"></NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
+          <NavbarToggler />
          
             <Nav className="ml-auto" navbar>
               <NavItem>
@@ -34,7 +34,7 @@ class NavHeader extends React.Component {
                 &nbsp;&nbsp;&nbsp;
               </NavItem>
               <NavItem className="mr-sm-2">
-                <Sigin buttonLabel="Signin" />
+
               </NavItem>
             </Nav>
           

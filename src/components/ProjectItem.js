a151d20c413d9724d3c25eb610dc65e7524a50fa
harshasanjeeva <harshasanjiva@gmail.com>
2018-03-06 01:08:04 -0800

    addding the search bar

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -12,6 +12,8 @@ import {
 } from 'material-ui/Table';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import ProjectItem from './ProjectItem'
 import NavHeader from './NavHeader';
+import SearchBar from 'material-ui-search-bar'
 class Work extends Component {
   render() {
     return (
       <div >
       <NavHeader />
+      
         <h3>Find work</h3>
+        <SearchBar
+        onChange={() => console.log('onChange')}
+        onRequestSearch={() => console.log('onRequestSearch')}
+        style={{
+          margin: '0 auto',
+          maxWidth: 800
+        }}
+      />
         <ProjectItem />
       </div>
     );
